751891666370e28ee53dddebc4e616ce4c6231d8
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-07-21 04:21:45 +0500

    chnages "

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
               <Home /> 
               <AvatarSlider/>
 
-              <ProductCard/>
+              <ProductCard/> 
               <Banner />
 
               {/* <Banner/> */}
