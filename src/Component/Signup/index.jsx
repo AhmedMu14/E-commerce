6eb6a9c8e683497034fbe708bc9a7bc2e76ba2d1
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-08-04 02:11:20 +0500

    chnages

--- src/Component/Signup/index.jsx
+++ src/Component/Signup/index.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 const SignUp = () => {
   const [firstName, setFirstName] = useState(""); 
   const [lastName, setLastName] = useState("");   
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");  
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
