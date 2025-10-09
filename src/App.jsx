81c72b2b82c2ac302f484f3dcaad218f6cc5d232
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-10-10 02:51:52 +0500

    not done yet

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <Route
           path="/"
           element={
-            <Layout>
+            <Layout>  
               <Home /> 
               <AvatarSlider/>
               <ProductCard/> 
