1de97d8af3cfbf470ecf6546f148345c061bf98b
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-11-08 00:21:04 +0500

    add

--- src/Component/MainComponents/ProductCard/index.jsx
+++ src/Component/MainComponents/ProductCard/index.jsx
@@ -1,48 +1,48 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
 
-function UsersList() {
-  const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(1);
+// function UsersList() {
+//   const [users, setUsers] = useState([]);
+//   const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const res = await axios.get(`http://localhost:5000/users?page=${page}`);
-      console.log(res.data)
-      setUsers(res.data); 
-    };
-    fetchUsers();
-  }, [page]);
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       const res = await axios.get(`http://localhost:5000/users?page=${page}`);
+//       console.log(res.data)
+//       setUsers(res.data); 
+//     };
+//     fetchUsers();
+//   }, [page]);
 
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-3">Users Page {page}</h2>
-      <ul>  
-        {users.map((u) => (
-          <li key={u._id}>
-            {u.name} - {u.email}
-          </li>
-        ))}
-      </ul>
+//   return (
+//     <div className="p-4">
+//       <h2 className="text-2xl font-bold mb-3">Users Page {page}</h2>
+//       <ul>  
+//         {users.map((u) => (
+//           <li key={u._id}>
+//             {u.name} - {u.email}   
+//           </li>
+//         ))}
+//       </ul>
 
-      <div className="mt-4 flex gap-2">
-        <button
-          disabled={page === 1}
-          onClick={() => setPage(page - 1)}
-          className="bg-gray-300 px-4 py-2 rounded"
-        >
-          Prev
-        </button>
+//       <div className="mt-4 flex gap-2">
+//         <button
+//           disabled={page === 1}
+//           onClick={() => setPage(page - 1)}
+//           className="bg-gray-300 px-4 py-2 rounded"
+//         >
+//           Prev
+//         </button>
 
-        <button
-          onClick={() => setPage(page + 1)}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
-}
+//         <button
+//           onClick={() => setPage(page + 1)}
+//           className="bg-blue-500 text-white px-4 py-2 rounded"
+//         >
+//           Next
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default UsersList;
+// export default UsersList;
