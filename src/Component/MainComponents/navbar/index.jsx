fd8dfdd7caa10a7c9d27d39e74d42e071337272b
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-12-02 13:38:52 +0500

    chnages not

--- src/Component/MainComponents/navbar/index.jsx
+++ src/Component/MainComponents/navbar/index.jsx
@@ -8,7 +8,7 @@ function index() {
 
       buffer matlab pdf images wagar a mongodb nhi bna aha ya chaezay store kay lia 
       AWS buket may image store ho te hai respose may url dey te hai  or wo ape database may save kar saktay ho 
-      ek ate ha cloudinary ya be response may url dey ta ha 
+      ek ate ha cloudinary ya be response may url dey  ta ha 
       </div>
   )
 }
